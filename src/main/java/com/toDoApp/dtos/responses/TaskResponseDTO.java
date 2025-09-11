42929dd0079bde5eace529709c0b8745794330b6
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-11 22:25:45 +0100

    created TaskReponseDTO

---
+++ src/main/java/com/toDoApp/dtos/responses/TaskResponseDTO.java
@@ -0,0 +1,26 @@
+package com.toDoApp.dtos.responses;
+
+import com.toDoApp.domain.models.Priority;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class TaskResponseDTO {
+    private String id;
+    private String userId;
+    private String title;
+    private String body;
+    private Priority priority;
+    private boolean completed;
+    private LocalDateTime dateCreated;
+    private LocalDateTime deadline;
+
+}
