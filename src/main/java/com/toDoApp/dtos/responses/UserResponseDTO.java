2da066af2112f9bf024867bbfbe8f27fdc25458b
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-11 21:57:12 +0100

    created UserResponseDTO

--- src/main/java/com/toDoApp/dtos/responses/UserResponseDTO.java
+++ src/main/java/com/toDoApp/dtos/responses/UserResponseDTO.java
@@ -1,4 +1,17 @@
 package com.toDoApp.dtos.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class UserResponseDTO {
+    private String id;
+    private String userName;
+    private String email;
 }
