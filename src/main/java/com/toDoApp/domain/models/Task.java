0c74c446878b24153b655c16e94a1e6dc26cf4f9
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-11 22:11:48 +0100

    created Task model

---
+++ src/main/java/com/toDoApp/domain/models/Task.java
@@ -0,0 +1,32 @@
+package com.toDoApp.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Task {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String title;
+    private String body;
+    private Priority priority;
+
+    private boolean completed;
+    private LocalDateTime dateCreated;
+    private LocalDateTime deadline;
+}
