a03c93b8072b8c0bbb3b448b0e231db0abd34d46
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-11 22:57:42 +0100

    Created Reminder Model

---
+++ src/main/java/com/toDoApp/domain/models/Reminder.java
@@ -0,0 +1,25 @@
+package com.toDoApp.domain.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "reminders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Reminder {
+    @Id
+    private String id;
+    private String taskId;
+    private LocalDateTime reminderTime;
+    private boolean sent;
+}
