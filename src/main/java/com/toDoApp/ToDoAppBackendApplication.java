19fabf84ceba8f0c240b78560b83082b7ea702c4
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-13 10:45:16 +0100

    created a Springboot main application

---
+++ src/main/java/com/toDoApp/ToDoAppBackendApplication.java
@@ -0,0 +1,11 @@
+package com.toDoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppBackendApplication.class, args);
+    }
+}
