122651acfe06a7f67b579710d220446052af6425
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-13 10:46:01 +0100

    created userRepo

--- src/main/java/com/toDoApp/domain/repositories/UserRepository.java
+++ src/main/java/com/toDoApp/domain/repositories/UserRepository.java
@@ -3,7 +3,13 @@ package com.toDoApp.domain.repositories;
 
 import com.toDoApp.domain.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends MongoRepository<User,String> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 
 }
