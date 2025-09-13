1d88b088cfe3d054a045f92e63f1ff5261a2f44c
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-13 10:42:17 +0100

    created reminderDTO

--- src/main/java/com/toDoApp/dtos/responses/ReminderResponseDTO.java
+++ src/main/java/com/toDoApp/dtos/responses/ReminderResponseDTO.java
@@ -1,7 +1,6 @@
 package com.toDoApp.dtos.responses;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,11 +9,10 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 
 public class ReminderResponseDTO {
-    public String id;
-    public String taskId;
-    public LocalDateTime reminderTime;
+    private String id;
+    private String taskId;
+    private LocalDateTime reminderTime;
     private boolean sent;
 }
