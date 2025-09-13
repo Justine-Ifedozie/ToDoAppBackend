6aaddd9335d5ef58490631ca11636ca14ece764b
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-13 11:34:49 +0100

    created ReminderRepository

---
+++ src/main/java/com/toDoApp/domain/repositories/ReminderRepository.java
@@ -0,0 +1,13 @@
+package com.toDoApp.domain.repositories;
+
+import com.toDoApp.domain.models.Reminder;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReminderRepository extends MongoRepository<Reminder,String> {
+    List<Reminder> findByTaskId(String taskId);
+    List<Reminder> findBySent(boolean sent);
+}
