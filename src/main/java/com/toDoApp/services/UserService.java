d6622ddcd4d6b2fe0d012b09163333490d430fc7
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-15 11:23:17 +0100

    created UserService

--- src/main/java/com/toDoApp/services/UserService.java
+++ src/main/java/com/toDoApp/services/UserService.java
@@ -3,12 +3,24 @@ package com.toDoApp.services;
 import com.toDoApp.dtos.requests.UserRequestDTO;
 import com.toDoApp.dtos.responses.UserResponseDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
+
     UserResponseDTO createUser(UserRequestDTO requestDTO);
+
+    Optional<UserResponseDTO> getUserById(String userId);
+
     Optional<UserResponseDTO> getUserByEmail(String email);
+
     Optional<UserResponseDTO> getUserByUsername(String username);
+
+    List<UserResponseDTO> getAllUsers();
+
+    UserResponseDTO updateUser(String userId, UserRequestDTO requestDTO);
+
+    void deleteUser(String userId);
 
     UserResponseDTO login(String email, String password);
 }
